191771343c487e5079e22c309a31fd0a82ad9623
temfa <akinfetemitope5@gmail.com>
2023-07-20 13:07:13 +0100

    Done

--- js/script.js
+++ js/script.js
@@ -168,9 +168,7 @@ let tempYear = tempDate.getFullYear();
 let tempMonth = tempDate.getMonth();
 let tempDay = tempDate.getDate();
 // months are ZERO index based;
-const futureDate = new Date(tempYear, tempMonth, tempDay + 14, 1, 15, 0);
-
-// let futureDate = new Date(2020, 3, 24, 11, 30, 0);
+const futureDate = new Date("07/30/2023");
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
